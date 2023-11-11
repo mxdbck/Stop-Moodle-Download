c30a740450faea532b82a58a73616764c81ad63a
maximedeboeck <119764381+maximedeboeck@users.noreply.github.com>
2023-11-11 11:02:36 +0100

    Handle redirects to forceDownload PDFs

--- content.js
+++ content.js
@@ -1,13 +1,46 @@
+// Handles direct PDF links by iterating through all links
+// with 'forcedownload=1' parameter and removing the parameter
+
 function removeForceDownloadParameter(link) {
   let url = new URL(link.href);
-  url.searchParams.delete('forcedownload'); // This removes the 'forcedownload' parameter
-  link.href = url.href; // Update the href attribute of the link
+  url.searchParams.delete('forcedownload');
+  link.href = url.href;
 }
 
-// Select all links on the page
 const links = document.querySelectorAll('a[href*="forcedownload=1"]');
 
-// Iterate over the links and remove the 'forcedownload' parameter
 links.forEach(link => {
+  console.log(link.href);
   removeForceDownloadParameter(link);
+});
+
+// Handles links with 'id=' parameter that redirect to PDFs
+// by adding a click event listener to each link and when
+// clicked, fetches the URL, removes the 'forcedownload'
+// parameter and opens the URL in a new tab.
+
+const allLinks = document.querySelectorAll('a');
+const linksWithId = Array.from(allLinks).filter(link => urlEndsWithId(link.href));
+
+function urlEndsWithId(url) {
+  const regex = /id=\d+$/; // This regex checks if a URL ends with 'id=' followed by numbers
+  return regex.test(url);
+}
+
+function handleClick(event) {
+
+  event.preventDefault();
+  let originalLink = event.currentTarget;
+
+  fetch(originalLink.href)
+    .then(response => {
+      let url = new URL(response.url);
+      url.searchParams.delete('forcedownload');
+      window.open(url);
+    })
+    .catch(error => console.log('Fetch error:', error));
+}
+
+linksWithId.forEach(link => {
+  link.addEventListener('click', handleClick);
 });
